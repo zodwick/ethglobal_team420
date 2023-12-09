1ac54badf47a7322be4dabb770a6c25bed2af7ef
zodwick <115868723+zodwick@users.noreply.github.com>
2023-12-09 16:00:37 +0530

    added scripting

--- scriptrun.py
+++ scriptrun.py
@@ -11,7 +11,7 @@ def jsruntime():
     for command in commands:
         try:
             # Run the command and capture the output
-            command = "cd stylus-as-example/ &&" + command
+            command = "cd stylus-as-example_js/ &&" + command
             result = subprocess.run(command, shell=True,
                                     check=True, text=True, capture_output=True)
 
@@ -30,3 +30,35 @@ def jsruntime():
             print("=" * 50)
 
 
+
+def rsruntime(example):
+    commands = [
+        "cargo stylus check",
+        "cargo stylus export-abi",
+        "cargo stylus deploy --private-key=7006c6ccdc4d7a46b7ac0007599730f287ca8e8ded9792a66a4102a99f084395",
+    ]
+    for command in commands:
+        try:
+            # Run the command and capture the output
+            if example == "hashing":
+                command = "cd stylus-as-example_rs/ && " +"cd hashing/ && "+ command
+            elif example == "voting":
+                command = "cd stylus-as-example_rs/ && " +"cd voting/ && "+ command
+            result = subprocess.run(command, shell=True,
+                                    check=True, text=True, capture_output=True)
+
+            # Print the command and its output
+            print(f"Command: {command}")
+            print("Output:")
+            print(result.stdout)
+            print("Error (if any):")
+            print(result.stderr)
+            print("=" * 50)
+
+        except subprocess.CalledProcessError as e:
+            # Handle if the command returns a non-zero exit code
+            print(f"Command: {command}")
+            print(f"Error: {e}")
+            print("=" * 50)
+
+rsruntime()
