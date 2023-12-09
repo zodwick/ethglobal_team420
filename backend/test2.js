6e71a408984f82914a1d0f25d77dc9c58bb4c320
jeffprakash <jeffprakash@gmail.com>
2023-12-09 23:43:34 +0530

    script for onchain

---
+++ backend/test2.js
@@ -0,0 +1,66 @@
+// Imports
+import { ethers } from 'ethers';
+
+// Constants
+let PROGRAM_ADDRESS;
+let RPC_URL;
+
+// Initial setup
+let stylusProvider;
+
+// Main function
+const main = async (inputNumber) => {
+  console.log('');
+  console.log('-----------------');
+  console.log('| Start program |');
+  console.log('-----------------');
+  console.log('');
+
+  console.log('-----------------');
+  console.log(`Input: ${inputNumber}`);
+  console.log('Calling program...');
+ 
+  RPC_URL = process.argv[2];
+  PROGRAM_ADDRESS = process.argv[3];
+
+  stylusProvider = new ethers.JsonRpcProvider(RPC_URL);
+
+  const resultBytes = await stylusProvider.call({
+    to: PROGRAM_ADDRESS,
+    data: ethers.toBeHex(inputNumber),
+  });
+  const result = parseInt(resultBytes, 16);
+
+  console.log(`Result: ${result}`);
+  console.log('-----------------');
+  console.log('');
+};
+
+////////////////
+// Init point //
+////////////////
+
+// Arguments check
+if (process.argv.length !== 4) {
+  console.log('Usage: npm run test:onchain <RPC_URL> <PROGRAM_ADDRESS> <inputNumber>');
+  process.exit(1);
+}
+
+console.log('*************************');
+console.log('* Stylus onchain tester *');
+console.log('*************************');
+console.log('');
+
+// Getting arguments
+
+const inputNumber = process.argv[4];
+
+// Initializing provider with provided RPC_URL
+
+
+main(inputNumber)
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
