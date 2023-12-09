fa02a30706bff5ea92892a3ef9581b567b589ce5
zodwick <115868723+zodwick@users.noreply.github.com>
2023-12-09 23:18:30 +0530

    Codegen API created

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,21 @@
 from flask import Flask
-
+from flask import request
+from codegen import CodeGen
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello, World!"
 
+
+# post request that accepts a text
+@app.route("/genCode", methods=["POST"])
+def post():
+    prompt = request.json["prompt"]
+    # example = calculate the square root of a number
+    language = request.json["language"]
+    Code = CodeGen(prompt,language)
+    return {"Code": Code}
+
+# @app.route
--- backend/codegen.py
+++ backend/codegen.py
@@ -9,7 +9,6 @@ load_dotenv()
 client = OpenAI()
 
 
-
 def extractCode(example):
     # Use regular expression to extract code between triple backticks
     code_pattern = re.compile(r'```([\s\S]+?)```')
@@ -18,36 +17,48 @@ def extractCode(example):
     # Concatenate and return the extracted code
     extracted_code = '\n'.join(matches)
     # remove the first line
-    extracted_code = extracted_code.split("\n",1)[1]
+    extracted_code = extracted_code.split("\n", 1)[1]
     return extracted_code
+
 
 def writeCodeToFile(code, filename):
     with open(filename, 'w') as file:
         file.write(code)
 
 
+def CodeGen(prompt: str, lang: str) -> str:
+    if lang == "js" or lang == "javascript" or lang == "ts" or lang == "typescript":
 
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "system",
+                    "content": systemReference,
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "system",
-            "content": systemReference,
-        
-        },
-        {
-            "role": "user",
-            "content": "can you write a similar implementation in main.ts for a function that finds the sum of all digits",
-        }
-    ],
-    model="gpt-3.5-turbo",
-    temperature=0.001,
-)
+                },
+                {
+                    "role": "user",
+                    "content": "can you write a similar implementation in main.ts for a function that"+prompt,
+                }
+            ],
+            model="gpt-4",
+            temperature=0.001,
+        )
+        codeContent = (chat_completion.choices[0].message.content)
+        Code = (extractCode(codeContent))
+        writeCodeToFile(
+            code=Code, filename="./stylus-as-example_js/assembly/app.ts")
 
-codeContent=(chat_completion.choices[0].message.content)
+        return Code
 
-
-Code=(extractCode(codeContent))
-
-
-writeCodeToFile(code=Code, filename="./stylus-as-example_js/assembly/app.ts")
+    elif lang == "rs":
+        if "hashing" in prompt.lower() or "hash" in prompt.lower():
+            with open("./stylus-as-example_rs/hashing/src/lib.rs", "r") as file:
+                data = file.read()
+                return data
+        else:
+            with open("./stylus-as-example_rs/voting/src/lib.rs", "r") as file:
+                data = file.read()
+                return data
+    else:
+        return "Language not supported yet"
